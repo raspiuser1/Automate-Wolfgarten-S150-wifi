997853130899bda419b0071c2d85624603b58c86
raspiuser1 <rob226@gmail.com>
2022-03-25 19:32:46 +0100

    Create config.h

---
+++ config.h
@@ -0,0 +1,28 @@
+// All neccessary configuration parameters are defined in this file.
+// Copy it to 'config.h' and make the neccessary changes there.
+// https://github.com/raspiuser1/Automate-Wolfgarten-S150-wifi
+// then upload your sketch to arduino
+// Replace the ... with the name and password of your WiFi
+#define SSID "ssid"
+#define PASSWORD "pass"
+
+
+// relais uitgangen voor apparatuur
+#define out1 16
+#define out2 5
+#define out3 12
+#define out4 0
+#define out5 2
+#define out6 13
+
+//D0 = GPIO16;
+//D1 = GPIO5;
+//D2 = GPIO4;
+//D3 = GPIO0;
+//D4 = GPIO2;
+//D5 = GPIO14;-
+//D6 = GPIO12;-
+//D7 = GPIO13;
+//D8 = GPIO15;
+//D9 = GPIO3;
+//D10 = GPIO1;
